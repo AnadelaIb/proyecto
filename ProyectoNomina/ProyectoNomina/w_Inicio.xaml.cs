8d628f25131fd3b9b051fb58110c6ef08afd2ff8
LizMaribel <vgaliz02@gmail.com>
2019-05-23 15:49:27 -0400

    Inicio de Sesión

---
+++ ProyectoNomina/ProyectoNomina/w_Inicio.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProyectoNomina
+{
+    /// <summary>
+    /// Lógica de interacción para w_Inicio.xaml
+    /// </summary>
+    public partial class w_Inicio : Window
+    {
+        NominaEntities datos;
+        public w_Inicio()
+        {
+            InitializeComponent();
+            datos = new NominaEntities();
+        }
+
+
+        private void btnIniciar_Click(object sender, RoutedEventArgs e)
+        {
+            var b = 0;
+            var categorias = datos.Usuario.ToList();
+            foreach (var c in categorias)
+            {
+
+                if (txtUsuario.Text.Equals(c.Usuario1) & txtPass.Password.Equals(c.Password))
+                {
+                    w_Menu ventana = new w_Menu();
+                    this.Close();
+                    ventana.ShowDialog();
+                   
+                    b = 1;
+                }
+
+
+            }
+            if (b == 0)
+            {
+                MessageBox.Show("Tus datos están incorrecto!");
+            }
+        }
+    }
+}
