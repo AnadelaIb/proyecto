d15ff3991fd329d01f26be4cbbea4d79ff148203
marlenego <marpaosim@hotmail.com>
2019-05-23 14:14:05 -0400

    Visualizar pedidos de vacaciones de los empleados

---
+++ ProyectoNomina/ProyectoNomina/w_vacaciones.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProyectoNomina
+{
+    /// <summary>
+    /// Lógica de interacción para w_vacaciones.xaml
+    /// </summary>
+    public partial class w_vacaciones : Window
+    {
+        NominaEntities datos;
+        public w_vacaciones()
+        {
+            InitializeComponent();
+            datos = new NominaEntities();
+        }
+        private void CargarDatosGrilla()
+        {
+            try
+            {
+                
+                dgVacaciones.ItemsSource = datos.Vacaciones.ToList();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            CargarDatosGrilla();
+        }
+    }
+}
