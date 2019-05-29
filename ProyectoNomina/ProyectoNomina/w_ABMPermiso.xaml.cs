0caf8bd09a15eb533c1033bbea8c388f5f1d162c
LizMaribel <vgaliz02@gmail.com>
2019-05-29 18:06:17 -0400

    Crear Permiso en proceso

---
+++ ProyectoNomina/ProyectoNomina/w_ABMPermiso.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProyectoNomina
+{
+    /// <summary>
+    /// Lógica de interacción para w_ABMPermiso.xaml
+    /// </summary>
+    public partial class w_ABMPermiso : Window
+    {
+        public w_ABMPermiso()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLimpiar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnEliminar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnModificar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnGuardar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
