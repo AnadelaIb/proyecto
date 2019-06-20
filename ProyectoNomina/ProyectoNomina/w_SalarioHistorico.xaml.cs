f66ad62e77a50c0fe16bba86bc32b4bf639f779b
AnadelaIb <annibac95@gmail.com>
2019-06-20 17:37:58 -0400

    Salario / registro histórico

---
+++ ProyectoNomina/ProyectoNomina/w_SalarioHistorico.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProyectoNomina
+{
+    /// <summary>
+    /// Lógica de interacción para w_SalarioHistorico.xaml
+    /// </summary>
+    public partial class w_SalarioHistorico : Window
+    {
+        NominaEntities datos;
+        public w_SalarioHistorico()
+        {
+            InitializeComponent();
+            datos = new NominaEntities();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            cboUsuario.ItemsSource = datos.Usuario.ToList();
+            cboUsuario.DisplayMemberPath = "Usuario";
+            cboUsuario.SelectedValuePath = "Id_Usuario";
+          
+        }
+
+        private void cboEmpleado_Selected(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
